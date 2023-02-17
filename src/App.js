77f8b7378e9f87889fdd64327c0bd6d7c704f831
James Kane <james.kane59@outlook.com>
2023-02-17 11:31:22 +0000

    rendering main components in app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+import Game from './components/Game';
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<>
+			<Header />
+			<Game />
+		</>
+	);
 }
 
 export default App;
