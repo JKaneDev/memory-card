b154189da9c158195067714828c1cc0b710de9b1
James Kane <james.kane59@outlook.com>
2023-02-17 11:32:11 +0000

    create body game component where cards will render

---
+++ src/components/Game.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Game = () => {
+	return <StyledGame></StyledGame>;
+};
+
+export default Game;
+
+const StyledGame = styled.div`
+	background-color: black;
+	min-height: 100vh;
+	color: white;
+`;
