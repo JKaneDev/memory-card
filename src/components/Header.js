d2d8b52d85c86591446863685da4c9bb2fb5f4ff
James Kane <james.kane59@outlook.com>
2023-02-17 11:31:46 +0000

    create header

---
+++ src/components/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Scoreboard from './Scoreboard';
+import abra from '../assets/abra.png';
+import styled from 'styled-components';
+
+const Header = () => {
+	return (
+		<StyledHeader>
+			<div>
+				<img src={abra} alt='title-img' id='title-img'></img>
+				<h1>Gotta Click 'em all</h1>
+			</div>
+			<Scoreboard />
+		</StyledHeader>
+	);
+};
+
+const StyledHeader = styled.div`
+	display: flex;
+	background-color: black;
+	padding: 1.5rem 1rem;
+	border-bottom: 2px solid yellow;
+
+	img {
+		height: 42px;
+		width: 56px;
+	}
+
+	h1 {
+		color: white;
+		letter-spacing: 0.1rem;
+	}
+
+	div {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+	}
+`;
+
+export default Header;
